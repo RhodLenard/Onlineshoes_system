16136e00fb62f95d82eeb536a48b5d60ef72f329
RhodLenard <rdelasnieves2023@student.nbscollege.edu.ph>
2025-02-16 18:52:38 +0800

    newest

--- function/logout.php
+++ function/logout.php
@@ -4,6 +4,8 @@ session_start();
 // Unset only the 'id' session (customer logout only)
 unset($_SESSION['id']);
 
-// Redirect to login or home page
-header("Location: ../index.php");
+echo '<script>
+    alert("You have successfully logged out.");
+    window.location.href = "../index.php"; // Redirect to index.php in the parent directory
+</script>';
 exit();
